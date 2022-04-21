c1da99bf376c83b78ce2ffa1222585def1a99214
Santanu <santanu_cst@yahoo.com>
2022-04-22 00:42:37 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spring-data-rest
 Spring Data Rest with Swagger 3, H2 Database and Flyway
+
+Link:
+https://docs.spring.io/spring-data/rest/docs/current/reference/html/
--- src/main/java/com/sdev/springdata/repository/CarManufacturerRepository.java
+++ src/main/java/com/sdev/springdata/repository/CarManufacturerRepository.java
@@ -6,21 +6,19 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.Optional;
+
 
 @Tag(name = "Car Manufacturers Resources")
 @RepositoryRestResource(collectionResourceRel = "carManufacturers", path = "car-manufacturers",
         exported = true)
 public interface CarManufacturerRepository extends PagingAndSortingRepository<CarManufacturerEntity, Long> {
 
-//    @Override
-//    @RestResource(path = "/all", exported = true, rel = "All Manufacturers Details")
-//    Iterable<CarManufacturerEntity> findAll();
-
-    @RestResource(exported = false)
     @Override
+    @RestResource(exported = false)
     void delete(CarManufacturerEntity entity);
 
-    @RestResource(exported = false)
     @Override
+    @RestResource(exported = false)
     <S extends CarManufacturerEntity> S save(S entity);
 }
